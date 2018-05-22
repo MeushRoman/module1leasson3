0421a4e930ace8277d7d6e9eb0d18fa96745fe05
test <test@238_11>
2018-05-22 10:00:43 +0600

    Add project files.

---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+void main()
+{   
+	
+	//printf("a) 5 10\tb) 100t\tc) x25\n   7sm\t  1949v\t  xy\n");
+
+	//int x, y;
+	//printf("X=");
+	//scanf("%d", &x);
+
+	//y = 7 * x * x - 3 * x + 6;
+	//printf("y = %d\n", y);
+
+	//int a,p;
+
+	//printf("a = ");
+	//scanf("%d", &a);
+
+	//p = a * 4;
+	//printf("p = %d\n", p);
+
+	//int r;
+	//float p = 3.14, d;
+	//printf("r = ");
+	//scanf("%d", &r);
+
+	//d = p*r*r;
+
+	//printf("d = %f\n", d);
+
+	int r = 6350, h;
+	float d;
+
+	printf("h = ");
+	scanf("%d")
+	
+
+
+}
